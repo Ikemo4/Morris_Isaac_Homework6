b13c07e44dea71b970b7f98e37d410ade3a2b19f
Ikemo4 <67703806+Ikemo4@users.noreply.github.com>
2021-02-25 00:08:37 -0700

    Add files via upload

---
+++ Models/BooklistDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Morris_Isaac_Homework5.Models
+{
+    //Create Booklist context class
+    public class BooklistDbContext : DbContext
+    {
+        public BooklistDbContext (DbContextOptions<BooklistDbContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
